830b09f42b638761340bb23a28caef2bf3f443f8
mazharul.miraz <mazharul.miraz@outlook.com>
2021-12-09 10:48:00 +0600

    First commit

---
+++ blogview/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def learn_django(request):
+    return HttpResponse('Hello Django')
+
+def learn_python(request):
+    return HttpResponse('<h1>Hello Python</h1>')
+
+def learn_var(request):
+    a = '<h1>hello world variable<h2>'
+    return HttpResponse(a)
+
+def learn_math(request):
+    a = 10+10
+    return HttpResponse(a)
