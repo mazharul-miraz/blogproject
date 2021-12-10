978e8d6051fff803b1d2ecefdff5cbf05dac55c9
mazharul.miraz <mazharul.miraz@outlook.com>
2021-12-10 22:04:27 +0600

    url pattern bug fixed

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -6,11 +6,11 @@ from blogview import views
 
 urlpatterns = [
      path('admin/', admin.site.urls),
-    # path('',views.home),
-    # path('learn_django/', views.learn_django ),
-    # path('learn_markup/', views.learn_markup ),
-    # path('learn_var/', views.learn_var ),
-    # path('learn_math/', views.learn_math ),
+    path('',views.home),
+    path('learn_django/', views.learn_django ),
+    path('learn_markup/', views.learn_markup ),
+    path('learn_var/', views.learn_var ),
+    path('learn_math/', views.learn_math ),
     path('cor/', include('course.urls')),
 
 ]
